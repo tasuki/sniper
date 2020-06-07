c9d02cef902aa6b3daceffe0ef2dd3dc6329c36e
Vit Brunner <vit.brunner@gmail.com>
2020-06-07 15:02:54 +0200

    Adaptible bloom filter length

---
+++ src/BloomFilter.elm
@@ -0,0 +1,69 @@
+module BloomFilter exposing (create, empty, read)
+
+import Array
+import Bloom
+
+
+
+-- settings
+
+
+falsePositives =
+    1 / 1000
+
+
+hashFunctions =
+    4
+
+
+
+-- implementation
+
+
+ln : Float -> Float
+ln x =
+    logBase e x
+
+
+exp : Float -> Float
+exp x =
+    e ^ x
+
+
+r : Float
+r =
+    -- as per https://hur.st/bloomfilter/
+    -hashFunctions / ln (1 - exp (ln falsePositives / hashFunctions))
+
+
+empty : Bloom.Filter
+empty =
+    Bloom.empty 1 hashFunctions
+
+
+create : List String -> Bloom.Filter
+create items =
+    let
+        itemCount =
+            List.length items |> toFloat |> max 3
+
+        bits =
+            -- as per https://hur.st/bloomfilter/
+            ceiling (itemCount * r)
+
+        bitsRoundedToSix =
+            6 * ceiling (toFloat bits / 6)
+
+        emptyBloomFilter =
+            Bloom.empty bitsRoundedToSix hashFunctions
+    in
+    List.foldr Bloom.add emptyBloomFilter items
+
+
+read : Array.Array Int -> Bloom.Filter
+read bits =
+    let
+        emptyFilter =
+            Bloom.empty (Array.length bits) hashFunctions
+    in
+    { emptyFilter | set = bits }
